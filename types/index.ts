0c3f8417bea4e9c04bc9d72f52d19a9a24db11c0
nuno <ngplbc@gmail.com>
2023-02-15 14:49:28 +0000

    feat: configure react-query to be used with ssr

---
+++ types/index.ts
@@ -0,0 +1,11 @@
+export type Quest = {
+  cover: string,
+  difficulty: number,
+  experience: number,
+  gold: number,
+  id: number,
+  skill: string,
+  skillTree: string,
+  title: string,
+  type: string,
+}
