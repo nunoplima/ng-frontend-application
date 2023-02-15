84d9e3f696df0e66a5a31a1fa0d2fdc835cacb66
nuno <ngplbc@gmail.com>
2023-02-15 14:49:28 +0000

    feat: configure react-query to be used with ssr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
-import GlobalStyle from '@/styles/GlobalStyles';
-import { darkTheme } from '@/styles/theme';
-import { QueryClient, QueryClientConfig, QueryClientProvider } from '@tanstack/react-query';
-import type { AppProps } from 'next/app';
-import Head from 'next/head';
-import { ThemeProvider } from 'styled-components';
+import { useRef } from 'react'
+import Head from 'next/head'
+import type { AppProps } from 'next/app'
+import {
+	Hydrate,
+	QueryClient,
+	QueryClientConfig,
+	QueryClientProvider
+} from '@tanstack/react-query'
+import { ThemeProvider } from 'styled-components'
+import { darkTheme } from '@/styles/theme'
+import GlobalStyle from '@/styles/GlobalStyles'
 
 const config: QueryClientConfig = {
 	defaultOptions: {
@@ -15,11 +21,11 @@ const config: QueryClientConfig = {
 			retryDelay: 500
 		}
 	}
-};
-
-const queryClient = new QueryClient(config);
+}
 
 export default function App({ Component, pageProps }: AppProps) {
+	const queryClient = useRef(new QueryClient(config))
+
 	return (
 		<>
 			<Head>
@@ -28,12 +34,14 @@ export default function App({ Component, pageProps }: AppProps) {
 				<meta name='robots' content='noindex' />
 			</Head>
 
-			<QueryClientProvider client={queryClient}>
-				<ThemeProvider theme={darkTheme}>
-					<GlobalStyle />
-					<Component {...pageProps} />
-				</ThemeProvider>
+			<QueryClientProvider client={queryClient.current}>
+				<Hydrate state={pageProps.dehydratedState}>
+					<ThemeProvider theme={darkTheme}>
+						<GlobalStyle />
+						<Component {...pageProps} />
+					</ThemeProvider>
+				</Hydrate>
 			</QueryClientProvider>
 		</>
-	);
+	)
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,38 @@
-import Head from 'next/head';
+import Head from 'next/head'
+import {
+  dehydrate,
+  QueryClient,
+  useQuery
+} from '@tanstack/react-query'
+import axios from 'axios'
+import { Quest } from '@types'
+
+const getQuests = async () => {
+	const { data } = await axios.get(`${process.env.BASE_URL || 'http://localhost:3000'}/api/quests`)
+
+	return data
+}
+
+export async function getStaticProps() {
+  const queryClient = new QueryClient()
+
+  await queryClient.prefetchQuery<Quest[]>(['quests'], getQuests)
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  }
+}
 
 export default function Home() {
+  const { data, isLoading } = useQuery<Quest[]>({
+		queryKey: ['quests'],
+		queryFn: getQuests
+	})
+
+	console.log({ isLoading })
+
 	return (
 		<>
 			<Head>
@@ -10,5 +42,5 @@ export default function Home() {
 
 			<main></main>
 		</>
-	);
+	)
 }
