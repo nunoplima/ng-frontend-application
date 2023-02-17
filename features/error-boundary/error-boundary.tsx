d3d6f512473d1e20ef396b14f9f78691ced0c331
nuno <ngplbc@gmail.com>
2023-02-17 03:41:51 +0000

    chore: add error boundary

---
+++ features/error-boundary/error-boundary.tsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { Props, State } from './types'
+
+class ErrorBoundary extends Component<Props, State> {
+  public state: State = {
+    hasError: false
+  }
+
+  public static getDerivedStateFromError(_: Error): State {
+    return { hasError: true }
+  }
+
+  public render() {
+    if (this.state.hasError) {
+      return <h1>Sorry.. there was an error</h1>
+    }
+
+    return this.props.children
+  }
+}
+
+export { ErrorBoundary }
